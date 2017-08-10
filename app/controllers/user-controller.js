a1579cbedcf1f582eb81a2c0c327ea794c9e59dc
cait-rocco <cait.rocco@gmail.com>
2017-08-10 17:08:39 -0500

    fixed login

--- app/controllers/user-controller.js
+++ app/controllers/user-controller.js
@@ -2,6 +2,8 @@
 
 gardenApp.controller('UserController', function($scope, $window, UserFactory){
 
+	$scope.isLoggedIn = false;
+
 	$scope.login = () => {
 		UserFactory.loginUser()
 		.then( (userData) => {
@@ -9,9 +11,23 @@ gardenApp.controller('UserController', function($scope, $window, UserFactory){
 		});
 	};
 
-	$scope.logout = () => {
-		UserFactory.logoutUser();
-		$window.location.href = '#!/';
-	};
+  // Listen for changes to auth state; If logged in, change isLoggedIn to true so
+  // the search input, add btn, and "logout" shows
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      $scope.isLoggedIn = true;
+      $scope.$apply();
+    } else {
+      $scope.isLoggedIn = false;
+      $scope.$apply();
+      $window.location.href = "#!/";
+    }
+  });
+
+  $scope.logout = () => {
+    UserFactory.logoutUser();
+    $window.location.href = '#!/';
+  };
+
 
 });
